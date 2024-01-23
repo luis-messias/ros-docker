642479c498483a34d890d2ea6ef7229d012c37cb
Luis Messias <luisfelipe_messias@hotmail.com>
2024-01-23 00:08:16 -0300

    Add mesa utils

--- Dockerfile
+++ Dockerfile
@@ -22,3 +22,5 @@ RUN apt-get update && apt-get install -y \
     libgl1-mesa-glx \
     libgl1-mesa-dri \
     && rm -rf /var/lib/apt/lists/*
+
+RUN apt-get update  && apt-get install mesa-utils
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 ./kill_all.sh
 
 xhost +
-docker run -it --net=host --gpus all\
+docker run -it --net=host --gpus all --privileged \
     --env="NVIDIA_DRIVER_CAPABILITIES=all" \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
